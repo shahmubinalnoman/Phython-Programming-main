ea30a3802bbb0f35202b392e606f8d65b66deba3
shahmubinalnoman <shahmubinalnoman45@gmail.com>
2024-12-10 20:56:27 +0600

    Add

---
+++ Hello.py
@@ -0,0 +1,13 @@
+import math
+
+print("Hello World")
+print(math.tan(45))
+print("The name of my country is Bangladesh.It's land area is 147570 square kilometers. It is a peaceful country..The people of the country wants to peace...But for many reasons,,It is hardly impossible to get it...")
+
+
+a=9
+b=4
+c=a+b
+print(c)
+print(a**b)
+print(a//b)
