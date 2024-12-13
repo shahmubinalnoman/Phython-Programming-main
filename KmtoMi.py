77aa32c2b9c6151cb5d306445d317ab44ee275e9
shahmubinalnoman <shahmubinalnoman45@gmail.com>
2024-12-13 10:31:15 +0600

    Commit

---
+++ KmtoMi.py
@@ -0,0 +1,7 @@
+# Firstly,input the required numbers and turend into the floating numbers
+km = float(input("Please enter the number of your choice into Kilometers:"))
+# Calculte the miles using the formula(1km=0.621371)
+miles = km * 0.621371
+# Print the result
+print("Your required result from Kilometers to Miles is:{}".format(miles))
+
---
+++ Swap.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+
+print("Before swap, both {} and {} are serially:{}:{}".format("a", "b", a, b))
+
+temp = a
+a = b
+b = temp
+
+
+print("After swap, both {} and {} are serially:{}:{}".format("a", "b", a, b))
